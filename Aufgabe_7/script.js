7fb3e02ddce17a6c483955c5982c521af979ae43
ankipfeffer <ann-kathrin.pfeffer@hs-furtwangen.de>
2021-11-30 17:55:18 +0100

    Create script.js
    A7

---
+++ Aufgabe_7/script.js
@@ -0,0 +1,34 @@
+var A07;
+(function (A07) {
+    window.addEventListener("load", function () {
+        var sound = [new Audio("assets/A.mp3"), new Audio("assets/C.mp3"), new Audio("assets/F.mp3"),
+            new Audio("assets/G.mp3"), new Audio("assets/hihat.mp3"), new Audio("assets/kick.mp3"),
+            new Audio("assets/laugh-1.mp3"), new Audio("assets/laugh-2.mp3"),
+            new Audio("assets/snare.mp3")];
+        function playbeat() {
+            setInterval(function () {
+                sound[4].play();
+            }, 2500);
+            setInterval(function () {
+                sound[5].play();
+            }, 1000);
+            setInterval(function () {
+                sound[8].play();
+            }, 500);
+        }
+        function playSample(sample) {
+            sample.play();
+        }
+        document.getElementById("obenlinks").addEventListener("click", function () { playSample(sound[0]); });
+        document.querySelector("#obenmitte").addEventListener("click", function () { playSample(sound[1]); });
+        document.getElementById("obenrechts").addEventListener("click", function () { playSample(sound[2]); });
+        document.getElementById("mittelinks").addEventListener("click", function () { playSample(sound[3]); });
+        document.getElementById("mittemitte").addEventListener("click", function () { playSample(sound[4]); });
+        document.getElementById("mitterechts").addEventListener("click", function () { playSample(sound[5]); });
+        document.getElementById("untenlinks").addEventListener("click", function () { playSample(sound[6]); });
+        document.getElementById("untenmitte").addEventListener("click", function () { playSample(sound[7]); });
+        document.getElementById("untenrechts").addEventListener("click", function () { playSample(sound[8]); });
+        document.getElementById("playbutton").addEventListener("click", playbeat);
+    });
+})(A07 || (A07 = {}));
+//# sourceMappingURL=script.js.map
