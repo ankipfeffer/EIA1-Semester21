4b2ef031fe2f2a296f0c6ab465e79c8928e49d88
ankipfeffer <ann-kathrin.pfeffer@hs-furtwangen.de>
2021-12-05 17:59:20 +0100

    Create script.ts
    A8

---
+++ Aufgabe_8/script/script.ts
@@ -0,0 +1,104 @@
+namespace A08{
+window.addEventListener("load", function () {
+
+// sounds auf dem Drumpad
+var sound: string[] = ["assets/A.mp3", "assets/C.mp3", "assets/F.mp3",
+                 "assets/G.mp3", "assets/hihat.mp3", "assets/kick.mp3",
+                "assets/laugh-1.mp3", "assets/laugh-2.mp3", "assets/snare.mp3"];
+
+var beatplay: string [] = [sound[4], sound[5], sound[8]];
+
+var index: number = 0;
+
+var beat;
+var beatRemix;
+
+// Drumpad Sounds abspielen
+function playSample (sample: string) {
+    var sounds: HTMLAudioElement = new Audio(sample);
+    sounds.play ();
+}
+
+// Beat der abspielt beim Playbutton
+
+
+// Playbutton Klick Reaktion
+function beatplays () {
+    if (document.getElementById("playbutton").getAttribute("class") == "fas fa-play") {
+        document.getElementById("playbutton").setAttribute("class", "fas fa-stop");
+        beat = setInterval(playbeat, 500);
+    }
+    else {
+        document.getElementById("playbutton").setAttribute("class", "fas fa-play");
+        clearInterval(beat);
+    }
+    function playbeat () {
+        playSample(beatplay[index]);
+        index++;
+        if (index == 2) {
+            index = 0;
+        }
+   }
+}
+
+// Zufälliger Sound auswählen durch Nummer für den Remix
+var min: number = 0;
+var max: number = 8;
+var remixnumber: number = Math.round(Math.random() * (max - min)) + min;
+
+
+console.log(remixnumber);
+
+// Remixbutton Klick Reaktion
+function remix () {
+    if (document.getElementById("remixbutton").getAttribute("class") == "fas fa-random") {
+        document.getElementById("remixbutton").setAttribute("class", "fas fa-stop");
+        beatRemix = setInterval(remixRechnung, 500);
+    }
+    else {
+        document.getElementById("remixbutton").setAttribute("class", "fas fa-random");
+        clearInterval(beatRemix);
+    }
+    function remixRechnung () {
+        beatplay = [];
+        beatplay.push(sound[remixnumber], sound[remixnumber + 1], sound[remixnumber + 2]);
+        
+        playSample(beatplay[index]);
+        index++;
+        if (index == 2) {
+            index = 0;
+        }
+    }
+}
+
+// um neuen Remix zu generieren, die Math.random Zahl ändern
+function deleteRemix () {
+    beatplay = [];
+    var remixnumber2: number = Math.round(Math.random() * (max - min)) + min;
+    remixnumber = remixnumber2;
+
+
+}
+
+
+// Aktionen bei Klick ausführen
+document.getElementById("obenlinks").addEventListener("click", function () { playSample(sound[0]); });
+document.querySelector("#obenmitte").addEventListener("click", function () { playSample(sound[1]); });
+document.getElementById("obenrechts").addEventListener("click", function () { playSample(sound[2]); });
+document.getElementById("mittelinks").addEventListener("click", function () { playSample(sound[3]); });
+document.getElementById("mittemitte").addEventListener("click", function () { playSample(sound[4]); });
+document.getElementById("mitterechts").addEventListener("click", function () { playSample(sound[5]); });
+document.getElementById("untenlinks").addEventListener("click", function () { playSample(sound[6]); });
+document.getElementById("untenmitte").addEventListener("click", function () { playSample(sound[7]); });
+document.getElementById("untenrechts").addEventListener("click", function () { playSample(sound[8]); });
+document.getElementById("playbutton").addEventListener("click", beatplays);
+document.getElementById("remixbutton").addEventListener("click", remix);
+document.getElementById("trashbutton").addEventListener("click", deleteRemix);
+
+
+
+
+
+
+});
+}
